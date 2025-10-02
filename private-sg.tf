8fd3363abc3ff67b812d1f1942fb323089b8d2b5
akashbalan <akashdharu1704@gmail.com>
2025-10-02 18:16:11 +0530

    changed security group

--- private-sg.tf
+++ private-sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_only" {
-  name        = "allow_all"
+  name        = "allow_only"
   description = "Allow all inbound traffic"
   vpc_id      = aws_vpc.default.id
 
@@ -10,21 +10,21 @@ resource "aws_security_group" "allow_only" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
   ingress {
     from_port   = 3389 #Remote Desktop (Windows)
     to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
   ingress {
     from_port   = 3306
     to_port     = 3306 #MySQL Database
     protocol    = "tcp"
-    cidr_blocks = ["10.2.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
 
--- sg.tf
+++ sg.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = aws_vpc.default.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -16,21 +16,21 @@ resource "aws_security_group" "allow_all" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
   ingress {
     from_port   = 3389 #Remote Desktop (Windows)
     to_port     = 3389
     protocol    = "tcp"
-    cidr_blocks = ["10.1.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
   ingress {
     from_port   = 3306
     to_port     = 3306 #MySQL Database
     protocol    = "tcp"
-    cidr_blocks = ["10.2.1.0/32"]
+    cidr_blocks = ["10.0.0.0/20"]
   }
 
 
